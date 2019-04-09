6202a086c230d347683664628828170a84bbab9e
Ashford M.R <m.r.ashford@swansea.ac.uk>
2019-04-09 09:23:49 +0100

    more harmony

--- src/app/harmony.service.ts
+++ src/app/harmony.service.ts
@@ -20,6 +20,7 @@ export class HarmonyService {
       const currentMelodyNoteIndex = keyRange.findIndex(k => k.note === currentMelodyNote.note);
       const previousMelodyNote = phrase[i - 1];
       const previousMelodyNoteIndex = keyRange.findIndex(k => k.note === previousMelodyNote.note);
+      const melodyIsLeap = Math.abs(currentMelodyNoteIndex - previousMelodyNoteIndex) > 1;
       const melodyChange = currentMelodyNoteIndex
         - previousMelodyNoteIndex;
       const previousHarmonyNoteIndex = harmony[i - 1];
@@ -51,8 +52,14 @@ export class HarmonyService {
       const stepwiseChoices = harmonyChoices.filter(choice => {
         return Math.abs(previousHarmonyNoteIndex - choice) <= 1;
       });
+      const leapChoices = harmonyChoices.filter(choice => {
+        return Math.abs(previousHarmonyNoteIndex - choice) > 1;
+      });
       const contraryMotionChoices = harmonyChoices.filter(choice => {
         return Math.sign(previousHarmonyNoteIndex - choice) !== Math.sign(melodyChange);
+      });
+      const parallelMotionChoices = harmonyChoices.filter(choice => {
+        return Math.sign(previousHarmonyNoteIndex - choice) === Math.sign(melodyChange);
       });
       const equalIsOk = harmonyChoices.includes(previousHarmonyNoteIndex);
 
@@ -60,18 +67,42 @@ export class HarmonyService {
       const stepAndContraryChoices = harmonyChoices.filter(h => contraryMotionChoices.includes(h) && stepwiseChoices.includes(h));
       if (stepAndContraryChoices.length > 0) {
         noteChoice = stepAndContraryChoices[0];
-      } else if (contraryMotionChoices.length > 0) {
-        noteChoice = contraryMotionChoices[0];
-      } else if (stepwiseChoices.length > 0) {
-        noteChoice = stepwiseChoices[0];
       } else {
-        noteChoice = Math.min(...harmonyChoices.map(h => Math.abs(h - previousHarmonyNoteIndex)));
+        if (melodyIsLeap) {
+          const stepAndParallelChoices = harmonyChoices.filter(h => parallelMotionChoices.includes(h) && stepwiseChoices.includes(h));
+          if (stepAndParallelChoices.length > 0) {
+            noteChoice = stepAndParallelChoices[0];
+          } else if (stepwiseChoices.length > 0) {
+            noteChoice = stepwiseChoices[0];
+          } else if (contraryMotionChoices.length > 0) {
+            noteChoice = contraryMotionChoices[0];
+          } else {
+            noteChoice = Math.min(...harmonyChoices.map(h => Math.abs(h - previousHarmonyNoteIndex)));
+          }
+        } else {
+          const leapAndParallelChoices = harmonyChoices.filter(h => parallelMotionChoices.includes(h) && leapChoices.includes(h));
+          if (leapAndParallelChoices.length > 0) {
+            noteChoice = leapAndParallelChoices[0];
+          } else if (contraryMotionChoices.length > 0) {
+            noteChoice = contraryMotionChoices[0];
+          } else if (stepwiseChoices.length > 0) {
+            noteChoice = stepwiseChoices[0];
+          } else {
+            noteChoice = Math.min(...harmonyChoices.map(h => Math.abs(h - previousHarmonyNoteIndex)));
+          }
+        }
       }
+
+
+
       harmony.push(noteChoice);
     }
     return harmony.map((h, i) => {
       const noteTone = new NoteTone();
       const note = keyRange[h];
+      if (!note && h !== -1) {
+        debugger;
+      }
       const melodyNote = phrase[i];
       if (h !== -1) {
         noteTone.note = note.note;
